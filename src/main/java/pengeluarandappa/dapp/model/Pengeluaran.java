64e55923855fa1d5dd4aa1e2218038d17d357d89
daffataufiq7 <daffataufiq7@gmail.com>
2025-05-04 20:36:39 +0700

    Hapus duplikat class Pengeluaran di model

--- src/main/java/pengeluarandappa/dapp/model/Pengeluaran.java
+++ src/main/java/pengeluarandappa/dapp/model/Pengeluaran.java
@@ -1,26 +1,22 @@
+package pengeluarandappa.dapp.model;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Pengeluaran {
     private double jumlah;
     private String keterangan;
-    private final String waktu;  // Ubah menjadi final
+    private final String waktu;
 
     public Pengeluaran(double jumlah, String keterangan) {
-        this.jumlah = jumlah;
+        this.jumlah     = jumlah;
         this.keterangan = keterangan;
-        this.waktu = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        this.waktu      = LocalDateTime
+                            .now()
+                            .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
     }
 
-    public double getJumlah() {
-        return jumlah;
-    }
-
-    public String getKeterangan() {
-        return keterangan;
-    }
-
-    public String getWaktu() {
-        return waktu;
-    }
+    public double getJumlah()      { return jumlah; }
+    public String getKeterangan() { return keterangan; }
+    public String getWaktu()      { return waktu; }
 }
